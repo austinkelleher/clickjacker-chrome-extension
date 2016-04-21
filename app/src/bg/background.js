9dc73b23a88d3c068ad4f31346f5f5f13e07a774
austinkelleher <austin.kell47@gmail.com>
2016-04-21 13:15:58 -0400

    Strip headers in a map.

--- app/src/bg/background.js
+++ app/src/bg/background.js
@@ -1,6 +1,12 @@
+var strippedHeaders = {
+  'x-frame-options': true,
+  'frame-ancestors': true
+};
+
 chrome.webRequest.onHeadersReceived.addListener(function (details) {
   for (var i = 0; i < details.responseHeaders.length; ++i) {
-    if (details.responseHeaders[i].name.toLowerCase() == 'x-frame-options') {
+    var header = details.responseHeaders[i].name.toLowerCase();
+    if (strippedHeaders[header]) {
       details.responseHeaders.splice(i, 1);
       return {
         responseHeaders: details.responseHeaders
