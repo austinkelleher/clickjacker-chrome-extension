d2d374f428c0c9ddc418f975f6616ff1c4a82535
austinkelleher <austin.kell47@gmail.com>
2016-04-21 12:55:25 -0400

    Working loading with fixed x-frame-options header.

--- app/js/calculator-extension-loader.js
+++ app/js/calculator-extension-loader.js
@@ -17,11 +17,60 @@ function injectScriptTag(src, id, onLoad) {
     (document.body || document.head).appendChild(script);
 }
 
-var CALCULATOR_ID = 'calculator-c-script';
-var scriptEl = document.getElementById(CALCULATOR_ID);
+injectScriptTag('//austinkelleher.github.io/js/test3.js', 'goteem', function() {
+    console.log('Injected..');
+});
 
-// Not yet injected
-if (!scriptEl) {
-    var calculatorExtensionURL = chrome.extension.getURL('js/calculator-extension.js');
-    injectScriptTag(calculatorExtensionURL, CALCULATOR_ID);
-}
+
+// function injectScriptTag(src, id, onLoad) {
+//     var script = document.createElement('script');
+//     script.src = src;
+//     script.type = 'text/javascript';
+//     script.id = id;
+//
+//     if (onLoad) {
+//         script.addEventListener('load', onLoad);
+//     }
+//
+//     (document.body || document.head).appendChild(script);
+// }
+//
+// var CALCULATOR_ID = 'calculator-c-script';
+// var scriptEl = document.getElementById(CALCULATOR_ID);
+//
+// // Not yet injected
+// if (!scriptEl) {
+//     var calculatorExtensionURL = chrome.extension.getURL('js/calculator-extension.js');
+//     injectScriptTag(calculatorExtensionURL, CALCULATOR_ID);
+// }
+
+// <b metadata="alert('hello')">
+
+
+// function get(url, callback) {
+//     var x = new XMLHttpRequest();
+//     x.onload = x.onerror = function() { console.log('X: ', x); callback(x.responseText); };
+//     x.open('GET', url);
+//     x.send();
+// }
+//
+// get('http://austinkelleher.com/js/test', function(code) {
+//     console.log('Goteem', code);
+//     if (!code) {
+//         code = "console.log('goteem')";
+//     }
+//
+//     var script = document.createElement('script');
+//     //script.src = 'http://austinkelleher.com/js/test.js';
+//     script.type = 'text/javascript';
+//     script.id = 'injected-lol';
+//     script.innerHTML = code;
+//
+//     // if (onLoad) {
+//     //     script.addEventListener('load', onLoad);
+//     // }
+//
+//     (document.body || document.head).appendChild(script);
+//
+//     //chrome.storage.local.set({lastUpdated: Date.now(), code: code});
+// });
